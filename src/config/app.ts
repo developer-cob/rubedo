cc8e361912872b775387d60c1e44cbc2831654d5
developer-cob <diamondcrowncob@gmail.com>
2024-06-19 17:37:47 -0500

    small changes

--- src/config/app.ts
+++ src/config/app.ts
@@ -6,7 +6,7 @@
 | This is the version of rubedo recommended to NOT CHANGE could cause bugs.
 |
 */
-export const VERSION = "3.1.0-beta";
+export const VERSION = "3.0.0-beta";
 
 /*
 |--------------------------------------------------------------------------
